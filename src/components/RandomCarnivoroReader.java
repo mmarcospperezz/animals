335c18b5a1b663fadb987c9c1e7e2b5f6233c720
mmarcospperezz <mmarcospperez2204@gmail.com>
2025-02-03 10:19:59 +0100

    animales

---
+++ src/components/RandomCarnivoroReader.java
@@ -0,0 +1,36 @@
+package components;
+
+import model.Carnivoro;
+
+import java.util.Random;
+
+public class RandomCarnivoroReader implements Reader<Carnivoro> {
+
+    private final static String[] ESPECIES = {
+            "Gato", "Perro", "Lobo", "leon", "Pantera", "Elefante"
+    };
+
+    private final static String[] NOMBRES = {
+            "Pinky", "Toby", "Alfredo", "Chasee", "Skie", "Ápolonia"
+    };
+
+    private final static String[] TIPODECARNE = {
+            "Blanca", "De cerdo", "De vacuno", "De ave"
+    };
+    private final Random random;
+
+    public RandomCarnivoroReader(Random random) {
+        this.random = random;
+    }
+
+    @Override
+    public Carnivoro read() {
+        return new Carnivoro(
+                ESPECIES[random.nextInt(ESPECIES.length)],
+                random.nextInt(20),
+                NOMBRES[random.nextInt(NOMBRES.length)],
+                TIPODECARNE[random.nextInt(TIPODECARNE.length)]
+            );
+
+    }
+}
---
+++ src/components/RandomHerbivoroReader.java
@@ -0,0 +1,34 @@
+package components;
+
+import model.Herbivoro;
+
+import java.util.Random;
+
+public class RandomHerbivoroReader implements Reader<Herbivoro> {
+    private final static String[] ESPECIES = {
+            "Gato", "Perro", "Lobo", "leon", "Pantera", "Elefante"
+    };
+
+    private final static String[] NOMBRES = {
+            "Pinky", "Toby", "Alfredo", "Chasee", "Skie", "Ápolonia"
+    };
+
+    private final static String[] CONJPLANTAS = {
+            "Flores", "Hierba", "Pasto", "Verduras"
+    };
+    private final Random random;
+
+    public RandomHerbivoroReader(Random random) {
+        this.random = random;
+    }
+
+    @Override
+    public Herbivoro read() {
+        return new Herbivoro(
+                ESPECIES[random.nextInt(ESPECIES.length)],
+                random.nextInt(20),
+                NOMBRES[random.nextInt(NOMBRES.length)],
+                CONJPLANTAS[random.nextInt(CONJPLANTAS.length)]
+        );
+    }
+}
