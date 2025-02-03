d12799b7aee349d1136e2a63e8ff45b710a65ced
mmarcospperezz <mmarcospperez2204@gmail.com>
2025-02-03 10:25:37 +0100

    animales

--- src/components/RandomAnimalReader.java
+++ src/components/RandomAnimalReader.java
@@ -1,4 +1,27 @@
 package components;
 
-public class RandomAnimalReader {
+import model.Animal;
+
+import java.util.Random;
+
+public class RandomAnimalReader implements Reader<Animal> {
+    private final static String[] ESPECIES = {
+            "Gato", "Perro", "Lobo", "leon", "Pantera", "Elefante"
+    };
+
+    private final static String[] NOMBRES = {
+            "Pinky", "Toby", "Alfredo", "Chasee", "Skie", "Ápolonia"
+    };
+
+    private final Random random;
+
+    public RandomAnimalReader(Random random) {
+        this.random = random;
+    }
+
+    @Override
+    public Animal read() {
+        return null;
+    }
 }
+
